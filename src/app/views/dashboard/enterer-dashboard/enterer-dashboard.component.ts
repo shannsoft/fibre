587a72525bcba6205062e07e506797e55a6764e4
adyasharani <52993095+adyasharani@users.noreply.github.com>
2020-05-08 11:59:59 +0100

    changes
    changes in form

--- src/app/views/dashboard/enterer-dashboard/enterer-dashboard.component.ts
+++ src/app/views/dashboard/enterer-dashboard/enterer-dashboard.component.ts
@@ -27,15 +27,33 @@ export class EntererDashboardComponent implements OnInit {
       block: [null, Validators.required],
       gp: [null, Validators.required],
       name: [null, Validators.required],
-      phoneno: [null, Validators.required],
-      emailid: [null, Validators.required],
+      phoneno : ['', Validators.compose([
+        Validators.required, Validators.pattern("[0-9]{10}")
+      ])],
+      emailid : ['', Validators.compose([
+        Validators.required, Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@+[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$')
+      ])],
       adress: [null, Validators.required],
       fiberrequired: [null, Validators.required],
+      fiberrequiredinmt:[null, Validators.required],
     });
    this.getdistrict();
    this.getBlock();
    this.getGp();
 
+  }
+  updateRequirement(){
+     var obj:any = {
+      "district" : this.entererForm.controls['district'].value,
+      "block" : this.entererForm.controls['block'].value,
+      "gp" : this.entererForm.controls['gp'].value,
+      "name" : this.entererForm.controls['name'].value,
+      "phoneno" : this.entererForm.controls['phoneno'].value,
+      "emailid" : this.entererForm.controls['emailid'].value,
+      "adress" : this.entererForm.controls['adress'].value,
+      "fiberrequired" : this.entererForm.controls['fiberrequired'].value,
+      "fiberrequiredinmt" : this.entererForm.controls['fiberrequiredinmt'].value,
+    }
   }
   getdistrict(){
 
